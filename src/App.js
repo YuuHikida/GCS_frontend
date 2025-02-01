63ca080f6b35c2cb00d7284a67c67a840e5e99ac
hikida <ponponda0103@gmail.com>
2025-02-01 16:04:15 +0900

    practice

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{ useEffect, useState } from 'react';
+
+
+
+function App() {
+  const[message,setMessage] = useState();
+
+  useEffect(()=>{
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/hello`)
+    .then((res)=>res.text())
+    .then(setMessage);
+  },[]);
+
+
+  return (
+    <div>
+      <h1>Hello Wolrd from Frontend</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
