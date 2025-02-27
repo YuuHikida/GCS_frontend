d134540a39b28d889507c8cda5cc7a218b49be56
hikida <ponponda0103@gmail.com>
2025-02-27 17:06:10 +0900

    ref

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,16 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
+      <ChakraProvider>
         <App />
+        </ChakraProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
