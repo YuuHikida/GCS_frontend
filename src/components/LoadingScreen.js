3144be844511097c7051aa1760f497ae49d653a0
hikida <ponponda0103@gmail.com>
2025-03-12 23:02:04 +0900

    お洒落なローディング画面作成中ver2

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -5,8 +5,11 @@ import './LoadingScreen.css';
 const LoadingScreen = () => {
   return (
     <div className="loading-container">
-      <div className="spinner"></div>
-      <p className="loading-text">Loading...</p>
+      <div className="spinner">
+        <div className="double-bounce1"></div>
+        <div className="double-bounce2"></div>
+      </div>
+      <p className="loading-text">Loading<span className="dots"></span></p>
     </div>
   );
 };
